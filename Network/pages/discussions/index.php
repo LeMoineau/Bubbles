a30f300d9f2ea03c1e804b661be12c1482ee042e
Pierrot Le Moineau <56643922+LeMoineau@users.noreply.github.com>
2020-06-03 13:12:40 +0200

    Add files via upload

---
+++ Network/pages/discussions/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Salut</title>
+  </head>
+  <body>
+
+    <h1>Coucou les amis</h1>
+
+  </body>
+</html>
